4a3581927dfd483fc50067393c2cd2506457fd49
villamar11 <56278168+villamar11@users.noreply.github.com>
2019-11-10 11:23:51 -0500

    Add files via upload

---
+++ venta.Gasolina/venta.Gasolina/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using venta.Gasolina.clases;
+namespace venta.Gasolina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CLiente informacioncliente = new CLiente("villamar", "lisseth", 1314844406,"montecristi");
+            GAsolina infogasolina = new GAsolina("Extra", 3,5);
+          
+
+
+            Console.WriteLine("  ***************FACTURA GASOLINA *************** ");
+            Console.WriteLine();
+            Console.WriteLine(" INGRESE LOS DATOS DEL CLIENTE  ");
+            Console.WriteLine("____________________________________");
+           
+
+            Console.Write(" APELLIDOS: ");
+            informacioncliente.APELLIDOS = Console.ReadLine();
+            Console.Write(" NOMBRES : "+" ");
+            informacioncliente.NOMBRE = Console.ReadLine();
+            Console.Write(" # CÉDULA: "+" ");
+            informacioncliente.CEDULA = int.Parse(Console.ReadLine());
+            Console.Write(" DIRECCIÓN: "+" ");
+            informacioncliente.DIRECCIÓN = Console.ReadLine();
+
+            Console.WriteLine(" ---------- INFORMACIÓN DE LA VENTA ---------- ");
+            Console.WriteLine(" TIPO DE GASOLINA INGRESE SEGUN DESEE:  EXTRA , SUPER  ");
+            infogasolina.TIPO = Console.ReadLine();
+            Console.WriteLine(" CANTIDAD DE GALONES:"+"   ");
+            infogasolina.GALONES = int.Parse(Console.ReadLine());
+            Console.WriteLine(" PRECIO POR GALÓN: " + infogasolina.PRECIO);
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine(  "   "+ "   " + "SUBTOTAL: " + infogasolina.SUBTOTAL);
+            Console.WriteLine("   " + "   " + "IVA: " + infogasolina.IVA);
+            Console.WriteLine("   " + "   " + " TOTAL: " + infogasolina.TOTALPAGO);
+            Console.WriteLine("   " + "   " + "GRACIAS PRO SU CONSUMO, TENGA UN BUEN DÍA ");
+
+
+
+        }
+        
+    }
+}
---
+++ venta.Gasolina/venta.Gasolina/clases/CLiente.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace venta.Gasolina.clases
+{
+    public class CLiente
+    {
+        private String apellidos;
+
+        public String APELLIDOS
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+        private String nombres;
+
+        public String NOMBRE
+        {
+            get { return nombres; }
+            set { nombres = value; }
+        }
+        private int cedula;
+
+        public int CEDULA
+        {
+            get { return cedula; }
+            set { cedula = value; }
+        }
+        private String dirección;
+
+        public String DIRECCIÓN
+        {
+            get { return dirección; }
+            set { dirección = value; }
+        }
+
+        public CLiente(string aPELLIDOS, string nOMBRE, int cEDULA, string dIRECCIÓN)
+        {
+            APELLIDOS = aPELLIDOS;
+            NOMBRE = nOMBRE;
+            CEDULA = cEDULA;
+            DIRECCIÓN = dIRECCIÓN;
+        }
+
+
+
+        // public CLiente(string apellidos, string nombres, int cedula, string dirección)
+        // {
+        //  this.apellidos = apellidos;
+        //this.nombres = nombres;
+        //this.cedula = cedula;
+        //this.dirección = dirección;
+        //}
+    }
+}
